244344d8aa52443639cf88f6df4d5c3e0c467e4d
Hero9696 <Miguelpadilla9619@gmail.com>
2025-05-08 17:17:49 -0600

    updates routes index.php

--- public/index.php
+++ public/index.php
@@ -5,17 +5,22 @@ require_once __DIR__ . '/../routes/routesWeb.php';
 
 $router = new Router();
 
+//Rutas para el AuthController
 $router->add('GET', '/', ['AuthController', 'login']);
 $router->add('POST', '/login', ['AuthController', 'autenticar']);
 $router->add('GET', '/registrer', ['AuthController', 'registrer']);
 $router->add('POST', '/register', ['AuthController', 'register']);
 
+//Rutas para el Dashboard
 $router->add('GET', '/dashboard', ['DashboardController', 'index']);
+
+
+//Rutas para Productos
 $router->add('GET', '/dashboard/productos', ['DashboardProductoController', 'dashboard']);
 $router->add('GET', '/productos', ['ProductoController', 'index']);
 $router->add('POST', '/guardar', ['ProductoController', 'guardar']);
 
-// rutas dinámicas (ejemplo)
+// Rutas Dinámicas Productos
 $router->add('GET', '/producto/editar/{id}', ['ProductoController', 'editar']);
 
 $method = $_SERVER['REQUEST_METHOD'];
